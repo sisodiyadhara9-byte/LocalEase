6b09a1acb19eb87ce66537b244fae9582c2ad386
sisodiyadhara9-byte <sisodiyadhara9@gmail.com>
2026-04-09 00:35:57 +0530

    Update urls.py
    fix urls remove HomeServices_app

--- HomeServices_project/urls.py
+++ HomeServices_project/urls.py
@@ -21,7 +21,6 @@ from HomeServices_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('HomeServices_app.urls')),
 ]
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
