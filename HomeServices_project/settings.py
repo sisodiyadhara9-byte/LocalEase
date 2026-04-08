84eec4ae15b14d899fb7df751a217d650bee5140
sisodiyadhara9-byte <sisodiyadhara9@gmail.com>
2026-04-09 01:21:28 +0530

    Update settings.py

--- HomeServices_project/settings.py
+++ HomeServices_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HomeServices_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'HomeServices_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
