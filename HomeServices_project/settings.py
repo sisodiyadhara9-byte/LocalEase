3e7ed9ab45d30dfa0866be0bda4d56009d991d1a
sisodiyadhara9-byte <sisodiyadhara9@gmail.com>
2026-04-09 00:48:30 +0530

    Update settings.py

--- HomeServices_project/settings.py
+++ HomeServices_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-p@o12h+fw!7=k6#l8o@6raux@y!g1u024bej^dd0%w!2$b@pn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localease.onrender.com']
 
 
 # Application definition
